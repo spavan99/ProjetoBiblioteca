8b8619c1493168a184a6e84e1c90c861d399dd0c
spavan99 <spavan@terra.com.br>
2021-08-05 18:33:19 -0300

    Acesso ao sistema e url somente logado

--- Controllers/EmprestimoController.cs
+++ Controllers/EmprestimoController.cs
@@ -11,6 +11,8 @@ namespace Biblioteca.Controllers
     {
         public IActionResult Cadastro()
         {
+
+            Autenticacao.CheckLogin(this);
             LivroService livroService = new LivroService();
             EmprestimoService emprestimoService = new EmprestimoService();
 
@@ -37,6 +39,7 @@ namespace Biblioteca.Controllers
 
         public IActionResult Listagem(string tipoFiltro, string filtro)
         {
+            Autenticacao.CheckLogin(this);
             FiltrosEmprestimos objFiltro = null;
             if(!string.IsNullOrEmpty(filtro))
             {
@@ -50,6 +53,7 @@ namespace Biblioteca.Controllers
 
         public IActionResult Edicao(int id)
         {
+            Autenticacao.CheckLogin(this);
             LivroService livroService = new LivroService();
             EmprestimoService em = new EmprestimoService();
             Emprestimo e = em.ObterPorId(id);
